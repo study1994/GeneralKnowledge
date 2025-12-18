8fcb1d2d31e40e9925209277289aabaa4ea1bd4e
zhao_study <zhuzhao1994@163.com>
2025-12-18 17:44:36 +0800

    gitadd

---
+++ git_add.sh
@@ -0,0 +1,6 @@
+# git config --global user.name '' 
+# git config --global user.email 'zhao_study19944d1274fabc3152e685c3b5edf6ccd66c@163.com'
+git config --global http.sslVerify "false"
+git add *
+git commit -m "gitadd"
+git push
